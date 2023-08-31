cc31767a1bae4a44919d687dabcda753aeb31791
shahid <shahidhashmiraj@gmail.com>
2023-08-31 13:43:47 +0530

    Tagged templete

--- JavaScript The Recent Parts/String/app.js
+++ JavaScript The Recent Parts/String/app.js
@@ -15,7 +15,7 @@ function formatCurrency(string,...value){
     var str = '';
     for(let i = 0; i< string.length;i++){
         if(i>0){
-            if(typeof value[i-1] =='numbeer'){
+            if(typeof value[i-1] =='number'){
                 str+= `$${value[i-1].toFixed(2)}`
             }else{
                 str += value[i-1];
@@ -26,3 +26,27 @@ function formatCurrency(string,...value){
     return str
 }
 console.log(msg);
+
+
+
+
+
+let names = 'shahid hashmi';
+let age = 19;
+let tool = 'javaScript';
+
+let intro = upper`hello ${names} your age is ${age} and your tool ${tool}`
+
+function upper(string,...value){
+  var ret = "";
+  for(let i =0; i<string.length;i++){
+    if(i>0){
+      ret += String(value[i-1]).toUpperCase();
+    }
+    ret += string[i]
+  }
+  return ret
+}
+console.log(intro);
+
+
