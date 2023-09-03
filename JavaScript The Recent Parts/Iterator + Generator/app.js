4b36d4c137c740dacfd7ca4871972d5cd939f13c
shahid <shahidhashmiraj@gmail.com>
2023-09-03 22:55:52 +0530

    custom Iterator

--- JavaScript The Recent Parts/Iterator + Generator/app.js
+++ JavaScript The Recent Parts/Iterator + Generator/app.js
@@ -1,5 +1,5 @@
-let str = "Hello"
-let str2 = [ 'H', 'e', 'l', 'l', 'o' ];
+let str = "Hello";
+let str2 = ["H", "e", "l", "l", "o"];
 
 let itemOne = str[Symbol.iterator]();
 console.log(itemOne.next()); // { value: 'H', done: false };
@@ -9,14 +9,40 @@ console.log(itemOne.next()); // { value: 'l', done: false };
 console.log(itemOne.next()); // { value: 'o', done: false };
 console.log(itemOne.next()); // { value: undefined, done: true };
 
-
 // objects not Iterbles
 
 let obj = {
-    a:1,
-    b:2,
-    c:3
+  a: 1,
+  b: 2,
+  c: 3,
 };
-for(let v in str2){
-    console.log(v);
+for (let v in str2) {
+  console.log(v);
 }
+
+let arr = [100, 200, 300];
+
+function IteretorFunction(values) {
+  let nextIndex = 0;
+  return {
+    next: function () {
+      if (nextIndex < values.length) {
+        return {
+          value: values[nextIndex++],
+          done: false,
+        };
+      } else {
+        return {
+          done: true
+        };
+      }
+    },
+  };
+}
+let iterators = IteretorFunction(arr);
+console.log(iterators.next());
+console.log(iterators.next());
+console.log(iterators.next());
+console.log(iterators.next());
+
+
