d4ba35baf4e83d6610199379bb892ef48eff1f55
shahid <shahidhashmiraj@gmail.com>
2023-10-05 16:20:25 +0530

    adding explaination of higher order function

--- JavaScript The Recent Parts/higher order function/app.js
+++ JavaScript The Recent Parts/higher order function/app.js
@@ -7,3 +7,6 @@ function higherOderFunction(){
 function callBackFunction(name){
     console.log(`name ${name}`);
 }
+
+
+// when a function take another function as a argument or it return another function as result is called higher order function 
