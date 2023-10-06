d9589271244c6764726a575c031ba1282ca0c3cc
shahid <shahidhashmiraj@gmail.com>
2023-10-07 01:18:24 +0530

    correcting higher order function

--- JavaScript The Recent Parts/higher order function/app.js
+++ JavaScript The Recent Parts/higher order function/app.js
@@ -1,12 +1,15 @@
-function higherOderFunction(){
-    let Myname = "shahid";
-    callBackFunction(names)
-
+function higherOrderFunction(callback) {
+    let myName = "Shahid";
+    callback(myName);
 }
 
-function callBackFunction(name){
-    console.log(`name ${name}`);
+function callBackFunction(name) {
+    console.log(`Name: ${name}`);
 }
+
+// Call the higher-order function and pass callBackFunction as an argument
+higherOrderFunction(callBackFunction);
+
 
 
 // when a function take another function as a argument or it return another function as result is called higher order function 
